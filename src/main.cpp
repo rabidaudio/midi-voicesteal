59202367672e0011d273b1d1804741646fc0c2b8
Charles Julian Knight <julian@fixdapp.com>
2021-02-07 20:04:16 -0500

    test in an embedded platform

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,20 @@
 // Copyright 2021 Charles Julian Knight
 #include <midimanager.h>
-#include <stdio.h>
-#include <assert.h>
 
-MidiManager<16, 2> m;
-
-void doMain() {
-  printf("size: %lu %lu %lu\n", sizeof(MidiManager<16, 2>), sizeof(size_t*), sizeof(size_t));
-  m.handle(0, 50, 127);
-  printf("%d, %d :: %d, %d\n", m.get(0).note, m.get(0).velocity, m.get(1).note, m.get(1).velocity);
-  m.handle(0, 45, 96);
-  printf("%d, %d :: %d, %d\n", m.get(0).note, m.get(0).velocity, m.get(1).note, m.get(1).velocity);
-  m.handle(0, 50, 63);
-  printf("%d, %d :: %d, %d\n", m.get(0).note, m.get(0).velocity, m.get(1).note, m.get(1).velocity);
-  m.handle(0, 45, 0);
-  printf("%d, %d :: %d, %d\n", m.get(0).note, m.get(0).velocity, m.get(1).note, m.get(1).velocity);
-  m.handle(0, 45, 96);
-  printf("%d, %d :: %d, %d\n", m.get(0).note, m.get(0).velocity, m.get(1).note, m.get(1).velocity);
-  m.handle(0, 50, 0);
-  printf("%d, %d :: %d, %d\n", m.get(0).note, m.get(0).velocity, m.get(1).note, m.get(1).velocity);
-  m.handle(0, 45, 0);
-  printf("%d, %d :: %d, %d\n", m.get(0).note, m.get(0).velocity, m.get(1).note, m.get(1).velocity);
-}
+MidiManager<16, 4> m;
 
 #ifdef ARDUINO
 #include <Arduino.h>
 void setup() {
-  doMain();
+  Serial.begin(9600);
+  Serial.println(sizeof(MidiManager<16, 4>), DEC);
 }
+
 void loop() {}
 #else  // ARDUINO
+#include <stdio.h>
+#include <assert.h>
 int main(int argc, char **argv) {
-    doMain();
     return 0;
 }
 #endif  // ARDUINO
